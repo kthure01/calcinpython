769bbdea78200d607ff081e1328f5021aafbb6a8
Kent <kent.ho.thureson@gmail.com>
2021-04-23 13:41:44 +0200

    updated

--- test_calculator.py
+++ test_calculator.py
@@ -3,5 +3,5 @@ from calculator import Calculator
 
 def test_add():
     assert 4 == Calculator.add(2, 2)
-    assert 4 == Calculator.add(2, 3)
+    assert 4 == Calculator.add(1, 3)
     assert Calculator.add(2, 3) == 5
