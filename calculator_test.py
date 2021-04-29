43184c6b36426c3fcbacac92c9e88a7a8be6c3c1
kthure01 <kent.ho.thureson@gmail.com>
2021-04-29 14:29:20 +0200

    u

--- calculator_test.py
+++ calculator_test.py
@@ -5,16 +5,17 @@ calc = Calculator()
 
 def test_add_method():
     assert calc.add(0, 1) == 1
-    assert Calculator.add(1, 0) == 1
-    assert Calculator.add(0, 2) == 2
-    assert Calculator.add(2, 0) == 2
-    assert Calculator.add(1, 1) == 2
-    assert Calculator.add(1, 2) == 3
-    assert Calculator.add(2, 1) == 3
+    assert 1 == Calculator.add(1, 0)
+    assert 2 == Calculator.add(0, 2)
+    assert 2 == Calculator.add(2, 0)
+    assert 2 == Calculator.add(1, 1)
+    assert 3 == Calculator.add(1, 2)
+    assert 3 == Calculator.add(2, 1)
+
 
 def test_subtract_method():
-    assert Calculator.subtract (0, 1) == -1
-    assert Calculator.subtract (0, 2) == -2
-    assert Calculator.subtract (1, 0) == 1
-    assert Calculator.subtract (2, 0) == 2
-    assert Calculator.subtract (2, 1) == 0
+    assert -1 == Calculator.subtract (0, 1)
+    assert -2 == Calculator.subtract (0, 2)
+    assert 1 == Calculator.subtract (1, 0)
+    assert 2 == Calculator.subtract (2, 0)
+    assert 0 == Calculator.subtract (2, 1)
